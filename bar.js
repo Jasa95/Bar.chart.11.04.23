50b30b6d9135a523ae3cfcc98dfd0a6551d73531
Jasa95 <jaco3020@stud.kea.dk>
2023-04-12 11:37:11 +0200

    Array og højde ændring på en søjle

--- bar.js
+++ bar.js
@@ -2,17 +2,19 @@
 
 window.addEventListener("load", startProgram);
 
+const num = [11, 16, 6, 29, 21, 12, 30, 9, 9, 29, 7, 2, 12, 3, 20, 26, 13, 22, 9, 25, 19, 1, 25, 31, 19, 17, 10, 8, 20, 24, 2, 25, 24, 11, 10, 14, 5, 12, 30, 13, 26,];
+
+
+
 function startProgram() {
-    console.log("java sccipt are  goarted"
-    
-    );
+    console.log("java sccipt are  goarted");
 
     const bars = document.querySelectorAll(".bar");
 
     const firstBar = bars[0];
 
-    const h= 50;
+    const h = num[0] / 32 * 100;
 
-    firstBar.style.height = `§{h}px`;
+    firstBar.style.height = `${h}px`;
 
-}
+};
