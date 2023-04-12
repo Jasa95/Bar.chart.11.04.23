c76474225c3f4a8bd6b0767a389e7c4672eabead
Jasa95 <jaco3020@stud.kea.dk>
2023-04-12 11:21:06 +0200

    Ændret højden på første søjle

--- bar.js
+++ bar.js
@@ -1,1 +1,18 @@
 "use strict"
+
+window.addEventListener("load", startProgram);
+
+function startProgram() {
+    console.log("java sccipt are  goarted"
+    
+    );
+
+    const bars = document.querySelectorAll(".bar");
+
+    const firstBar = bars[0];
+
+    const h= 50;
+
+    firstBar.style.height = `§{h}px`;
+
+}
